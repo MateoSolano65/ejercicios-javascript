0c139e9e25cac48e6236b13148d0fa5f7ab599e5
Mateo Solano <mateosolano65@gmail.com>
2024-05-09 20:30:03 -0500

    first commit

---
+++ src/ejercicios/ejercicio-1.js
@@ -0,0 +1,15 @@
+//  1. Contraseña válida
+
+// Escribir una función llamada `contrasenaValida` que reciba 
+// un string y retorne `true` si el string 
+// es igual a "2Fj(jjbFsuj" o "eoZiugBf&g9". De lo contrario debe retornar `false`.
+
+const contrasenaValida = ()=>{}
+
+// escribe tu respuesta acá
+
+// código de prueba
+console.log(contrasenaValida("2Fj(jjbFsuj")) // true
+console.log(contrasenaValida("eoZiugBf&g9")) // true
+console.log(contrasenaValida("hola")) // false
+console.log(contrasenaValida("")) // false
