5806e2f4f1182d172c491ca5b291401b663dc349
Mateo Solano <mateosolano65@gmail.com>
2024-05-10 00:41:40 -0500

    ejercicio 2 finished

--- src/ejercicios/ejercicio-2.js
+++ src/ejercicios/ejercicio-2.js
@@ -16,6 +16,8 @@ const calcularImpuestos = ( edad, ingresos ) => {
 
 
 
+
+
 // const calcularImpuestos = ( edad, ingresos)=>{
 //     if (edad >= 18 && ingresos >= 1000) {
 //         return ingresos * 0.40
